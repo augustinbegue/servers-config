d1bd985c574d46d8296b77db472d3e3d65f1a34b
Augustin <augustin@begue.cc>
2022-09-27 11:23:18 +0000

    -y

--- maas/deploy-scripts/postgres.sh
+++ maas/deploy-scripts/postgres.sh
@@ -1,11 +1,11 @@
 # Get Postgres Official repo
-apt install wget ca-certificates --allow
+apt install wget ca-certificates -y
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 
 # Install Postgres
 apt update
-apt install postgresql postgresql-contrib --allow
+apt install postgresql postgresql-contrib -y
 service postgresql start
 
 # Set Default Password
