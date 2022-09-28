d5acf3e34a92e1dee02dc2c65b85403e9775de8c
Augustin BEGUE <augustin@begue.cc>
2022-09-28 09:19:23 +0000

    node deploy script

--- maas/deploy-scripts/node.sh
+++ maas/deploy-scripts/node.sh
@@ -1,6 +1,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install 16.17.1
 nvm use 16.17.1
-source ~/.bashrc
 npm install -g npm@latest
